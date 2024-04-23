eefd98fafab22798642a519e95afc15c145a1cff
C4Confidence <iamconfidence@gmail.com>
2024-04-23 15:00:24 +0100

    Installs a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,17 @@
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+#script to install flask using pip3
+
+package { ['python-pip']:
+  ensure  => installed,
 }
-package {'werkzeug':
-    ensure => '2.1.1',
-    provider => 'pip3',
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python-pip']
+}
+
+package { 'Flask':
+  ensure   =>  '2.1.0',
+  provider =>  'pip3',
+  require  => Package['python-pip']
 }
