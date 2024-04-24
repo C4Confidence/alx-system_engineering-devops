d3655e19bd1f58e63411cceb3093fe4b75cc39bd
C4Confidence <iamconfidence@gmail.com>
2024-04-24 21:19:41 +0100

    Don't kill me o

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,6 @@
 # kill process killmenow
 
-exec { 'kill_killmenow_process':
-  command     => 'pkill killmenow',
-  refreshonly => true,
-  onlyif      => 'pgrep killmenow',
-  provider    => 'shell',
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
