82c8fe945d2611bfda16ca50497a6804b32b6e09
C4Confidence <iamconfidence@gmail.com>
2024-04-24 17:32:06 +0100

    Installs a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -7,11 +7,11 @@ package { ['python3-pip']:
 package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
-  require  => Package['python-pip']
+  require  => Package['python3-pip']
 }
 
 package { 'Flask':
   ensure   =>  '2.1.0',
   provider =>  'pip3',
-  require  => Package['python-pip']
+  require  => Package['python3-pip']
 }
