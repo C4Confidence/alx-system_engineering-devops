e04c577c1cd546a7b58c50dc581a87c7ddb926a6
C4Confidence <iamconfidence5@gmail.com>
2024-07-03 16:58:19 +0100

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit."""
-
+"""
+    Uses reddit API to get 10 hot posts
+"""
 import requests
 
-def top_ten(subreddit):
-    """Print the titles of the 10 hottest post on a given Subreddit"""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "C4Confidence/1.0.0"
-            }
-    params = {
-            "limit": 10
-            }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c  in results.get("children")]
 
+def top_ten(subreddit):
+    """Get 10 hot posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'user-agent': 'request'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json().get("data").get("children")
+    top_10_posts = "\n".join(post.get("data").get("title") for post in data)
+    print(top_10_posts)
