0e36a929baa173a836720b0522cfb4ae06e04dc3
C4Confidence <iamconfidence5@gmail.com>
2024-07-03 13:53:18 +0100

    How many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,17 +7,13 @@ import requests
 def number_of_subscribers(subreddit):
     """code to get the number of subscribers in a subreddit"""
 
-    base_url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'C4Confidence/1.0.0'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+            "User-Agent": "C4Confidence/1.0.0"
+            }
 
-    requested = requests.get(base_url, headers=headers, allow_redirects=False)
-
-    if requested.status_code != 200:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-    r_json = requested.json()
-
-    data = r_json.get("data")
-
-    subscribers = data.get("subscribers")
-    return subscribers
+    results = response.json().get("data")
+    return results.get("subscribers")
